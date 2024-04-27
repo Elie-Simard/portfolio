2546a96d3098b7c21af35e192c557ccd592cd7f0
FizboZ <elie_simard@live.fr>
2024-04-27 13:54:43 -0400

    foot

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer(props) {
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
           Made with <span role="img">❤️</span> by {greeting.title}
-          <br /> Inspired by Ashutosh Hathidara's Opensource Portfolio
+          <br /> Based on Ashutosh Hathidara's Opensource Portfolio
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle} /> */}
       </Fade>
